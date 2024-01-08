429308f35f6fa1bda495a84019966fb7164603ed
rpdNick <nickyovchenko@gmail.com>
2024-01-08 18:29:34 +0200

    info panel data

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,6 +2,7 @@ import "./Main.scss";
 import Counter from '../Counter/Counter';
 
 const Main = (props) => {
+  console.log(props)
   return (
     <main className="container">
       <div className="main_content">
@@ -14,11 +15,11 @@ const Main = (props) => {
           <div className="description">{props.data.description.text}</div>
           <div className="info_panel">
             <div className="info_item location">
-              <img className="icon" src="" alt="icon" />
+              <img className="icon" src={props.data.panel.location.icon} alt="icon" />
               <span>{props.data.panel.location.name}</span>
             </div>
             <div className="info_item date">
-              <img className="icon" src="" alt="icon" />
+              <img className="icon" src={props.data.panel.date.icon} alt="icon" />
               <span>{props.data.panel.date.datetime}</span>
             </div>
           </div>
