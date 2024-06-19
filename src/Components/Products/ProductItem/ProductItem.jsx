199a339e6a1a1356da5cb2184d5bb3892a894d83
rpdNick <nickyovchenko@gmail.com>
2024-06-19 11:22:05 +0300

    product item component

--- src/Components/Products/ProductItem/ProductItem.jsx
+++ src/Components/Products/ProductItem/ProductItem.jsx
@@ -5,26 +5,36 @@ import "./ProductItem.scss";
 
 const ProductItem = () => {
   let [product, setProduct] = useState(null);
-  let {productId} = useParams();
-  console.log(productId)
+  let { productId } = useParams();
+  console.log(productId);
 
-  useEffect(()=> {
-    axios.get(`https://dummyjson.com/products/${productId}`)
+  useEffect(() => {
+    axios
+      .get(`https://dummyjson.com/products/${productId}`)
       .then((response) => {
         setProduct(response.data);
-        console.log(response.data)
+        console.log(response.data);
       });
-    }, []);
+  }, []);
 
-    if (!product) {
-      return <div>Loading...</div>;
-    }
+  if (!product) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="ProductItem container">
-      <div className="product_tetle">Product Item</div>
-      <div className="product_name">{product.title}</div>
-      <img src={product.images[0]} alt="product" />
+      <div className="product_header">
+        <div className="product_brand">{product.brand}</div>
+        <div className="product_rating">Rating: {product.rating}</div>
+      </div>
+      <div className="columns_container">
+        <div className="column_wrap">
+          <div className="product_name">{product.title}</div>
+          <img className="product_image" src={product.images[0]} alt="product" />
+          <div className="price">{product.price}$</div>
+        </div>
+        <div className="column_wrap">{product.description}</div>
+      </div>
     </div>
   );
 };
